fc6328ae23cb90f6cffa1d9d063cf4a9f812d5a4
sandalf <sandalf@faxbois.fi>
2021-12-14 17:18:59 +0200

    Change XML parser

--- xml_parser.py
+++ xml_parser.py
@@ -38,12 +38,12 @@ weights = [1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.2
 #    req.urlretrieve("https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2019-11-29&endtime=2020-11-28", "input/temp" + city + "3.xml")
 #    req.urlretrieve("https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2020-11-29&endtime=2021-11-28", "input/temp" + city + "4.xml")
 
-for city in cities:
-    os.system("wget 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2016-11-29&endtime=2017-11-28' -O input/temp" + city + "0.xml")
-    os.system("wget 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2017-11-29&endtime=2018-11-28' -O input/temp" + city + "1.xml")
-    os.system("wget 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2018-11-29&endtime=2019-11-28' -O input/temp" + city + "2.xml")
-    os.system("wget 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2019-11-29&endtime=2020-11-28' -O input/temp" + city + "3.xml")
-    os.system("wget 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2020-11-29&endtime=2021-11-28' -O input/temp" + city + "4.xml")
+#for city in cities:
+#    os.system("wget 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2016-11-29&endtime=2017-11-28' -O input/temp" + city + "0.xml")
+#    os.system("wget 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2017-11-29&endtime=2018-11-28' -O input/temp" + city + "1.xml")
+#    os.system("wget 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2018-11-29&endtime=2019-11-28' -O input/temp" + city + "2.xml")
+#    os.system("wget 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2019-11-29&endtime=2020-11-28' -O input/temp" + city + "3.xml")
+#    os.system("wget 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + city + "&starttime=2020-11-29&endtime=2021-11-28' -O input/temp" + city + "4.xml")
 
 
 content = []
@@ -58,8 +58,10 @@ for i, city in enumerate(cities):
 dates = []
 temps = []
 k = 0
+dicts = []
 
 for city in files:
+    d = {}
     for filename in city:
         # Read the XML file
         with open(filename, "r") as file:
@@ -82,6 +84,7 @@ for city in files:
                         dates2.append(list(element.children)[3].text.split("T")[0])
                         # Parse average temperature
                         temps2.append(list(element.children)[7].text)
+                        d[list(element.children)[3].text.split("T")[0]] = list(element.children)[7].text
 
             #dates2.reverse()
             #temps2.reverse()
@@ -89,39 +92,28 @@ for city in files:
                 dates.extend(dates2)
             temps.append(temps2)
         k += 1
+    dicts.append(d)
 
 #dates.reverse()
 #temps.reverse()
 
+days = dicts[0].keys()
+avg_temps = []
+for day in days:
+    sum_temp = 0
+    num = 0
+    for i, d in enumerate(dicts):
+        temp = d.get(day)
+        if temp is not None:
+            if temp != "NaN":
+                sum_temp = sum_temp + float(temp) * weights[i]
+                num = num + weights[i]
+    avg_temps.append(sum_temp / num)
+    #print(day + ": " + str(sum_temp / num) + " " + str(sum_temp) + " " + str(num))
 
-temps2 = [[]] * 24
-i = 0
-j = 0
-for temp in temps:
-    if i == 0:
-        temps2[j] = temp
-        i += 1
-    elif i == 4:
-        temps2[j].extend(temp)
-        i = 0
-        j += 1
-    else:
-        temps2[j].extend(temp)
-        i += 1
-temps = temps2
-
-for i, city in enumerate(temps):
-    for j, value in enumerate(city):
-        temps[i][j] = float(temps[i][j])
-
-temps_avg = [0] * len(temps[0])
-for i, city in enumerate(temps):
-    for j, value in enumerate(city):
-        temps_avg[j] = temps_avg[j] + value * weights[i]
-
-for i in range(len(temps_avg)):
-    temps_avg[i] = temps_avg[i] / 33
+print(len(days))
+print(len(avg_temps))
 
 with open("temperatures.txt", "w") as file:
-    for date, temp in zip(dates, temps_avg):
+    for date, temp in zip(days, avg_temps):
         file.write(date + ", " + str(temp) + "\n")
